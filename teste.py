ab89d1c10a5a5b9391d4517d4a2c909c68a0419c
ubahkl <ubahkl@gmail.com>
2021-05-23 19:21:12 -0300

    adicionando funcao na brach de soma

--- teste.py
+++ teste.py
@@ -1,4 +1,7 @@
 
+def soma(n1,n2):
+    n3 = n1 + n2
+    return n3 
 
 
 
@@ -6,3 +9,6 @@
 n1 = float(input("Digite  o número 1 : \n"))
 n2 = float(input("Digite  o número 2 : \n"))
 
+resp = soma(n1,n2)
+
+print (resp)
