c0c5c6b5ae75340434f0602a626763539fa42f3a
ubahkl <ubahkl@gmail.com>
2021-05-23 19:27:46 -0300

    adicionando a funcao subtração

--- teste.py
+++ teste.py
@@ -1,8 +1,14 @@
 
 
 
-
+def subtracao(n1,n2):
+    n3 = n1 + n2
+    return n3
 
 n1 = float(input("Digite  o número 1 : \n"))
 n2 = float(input("Digite  o número 2 : \n"))
+
+resp1 = subtracao(n1,n2)
+
+print (resp1)
 
